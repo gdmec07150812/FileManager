aea9a92a12b5d7edfb9117b4f57abb0e9b4f7b79
jacky <499042532>
2017-03-27 21:58:32 +0800

    first

--- app/src/main/java/com/gdmec/jacky/filemanager/FileAdapter.java
+++ app/src/main/java/com/gdmec/jacky/filemanager/FileAdapter.java
@@ -1,8 +1,11 @@
 package com.gdmec.jacky.filemanager;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.File;
 
@@ -14,6 +17,18 @@ public class FileAdapter extends MyAdapter<File> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if(convertView==null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item,null);
+        }
+        ImageView imageView = MyViewHolder.get(convertView,R.id.imageview);
+        TextView textview =  MyViewHolder.get(convertView,R.id.textview);
+        File file = getDataList().get(position);
+        if(file.isDirectory()){
+            imageView.setImageResource(R.drawable.folder);
+        }else{
+            imageView.setImageResource(R.drawable.file);
+        }
+        textview.setText(file.getName());
+        return convertView;
     }
 }
--- app/src/main/java/com/gdmec/jacky/filemanager/MainActivity.java
+++ app/src/main/java/com/gdmec/jacky/filemanager/MainActivity.java
@@ -1,13 +1,42 @@
 package com.gdmec.jacky.filemanager;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final File ROOT = Environment.getExternalStorageDirectory();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+
+        FileAdapter fileAdapter = new FileAdapter(this);
+
+        List<File> fileList = new ArrayList<>();
+
+        File[] files = ROOT.listFiles();
+
+        for (File temp : files) {
+
+            fileList.add(temp);
+
+        }
+
+        fileAdapter.setDataList(fileList);
+
+        listView.setAdapter(fileAdapter);
+
     }
 }
